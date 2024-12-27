1ef58398d9db05a28d2534083f4cd3a0b03a9a02
athayanaufal <133175628+athayanaufal@users.noreply.github.com>
2024-12-27 20:39:57 +0700

    Update web.php

--- TubesWAD/routes/web.php
+++ TubesWAD/routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\MilihKamarController;
+use App\Http\Controllers\RiwayatMedisController;
+use App\Http\Controllers\KamarController;
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/kamar', [MilihKamarController::class, 'index'])->name('pemilihan_kamar.index');
+Route::post('/kamar/pilih/{pasien_id}', [MilihKamarController::class, 'pilihKamar'])->name('pemilihan_kamar.pilih');
+
+Route::middleware('dokter')->group(function () {
+    Route::get('/pasien/{pasien_id}/riwayat-medis', [RiwayatMedisController::class, 'index'])->name('riwayat_medis.index');
+    Route::post('/pasien/{pasien_id}/riwayat-medis', [RiwayatMedisController::class, 'store'])->name('riwayat_medis.store');
+});
+
+Route::prefix('kamar')->group(function () {
+    Route::get('/', [KamarController::class, 'index'])->name('kamar.index'); // Menampilkan daftar kamar
+    Route::get('/create', [KamarController::class, 'create'])->name('kamar.create'); // Form tambah kamar
+    Route::post('/', [KamarController::class, 'store'])->name('kamar.store'); // Simpan data kamar
+    Route::get('/{id}/edit', [KamarController::class, 'edit'])->name('kamar.edit'); // Form edit kamar
+    Route::put('/{id}', [KamarController::class, 'update'])->name('kamar.update'); // Update data kamar
+    Route::delete('/{id}', [KamarController::class, 'destroy'])->name('kamar.destroy'); // Hapus kamar
 });
