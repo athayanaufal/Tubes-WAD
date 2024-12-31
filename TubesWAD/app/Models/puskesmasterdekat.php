46eea00f6d095ab0987a2a4c681960c5f071c4a6
nickk <148069949+nickkenvy@users.noreply.github.com>
2024-12-31 22:50:08 +0700

    Update puskesmasterdekat.php

--- TubesWAD/app/Models/puskesmasterdekat.php
+++ TubesWAD/app/Models/puskesmasterdekat.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class puskesmasterdekat extends Model
+class puskesmasterdekat extends Model  
 {
     use HasFactory;
 
-    protected $table = 'puskesmas';
+    protected $table = 'puskesmas_terdekat';  
 
     protected $fillable = [
         'nama',
